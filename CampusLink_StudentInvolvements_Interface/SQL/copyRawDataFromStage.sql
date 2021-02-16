a4b6136b57d7e44ae232a4405ec60782ffb49045
cecameron <ccameron@osugiving.com>
2021-02-15 19:46:53 -0600

    Update StudentInvolvements Interface

--- CampusLink_StudentInvolvements_Interface/SQL/copyRawDataFromStage.sql
+++ CampusLink_StudentInvolvements_Interface/SQL/copyRawDataFromStage.sql
@@ -1,35 +1,54 @@
 CREATE OR REPLACE TABLE "01_NewData"
 (
-  "CWID"                         varchar(10)
-, "Preferred_FName"              varchar(150)
-, "FirstName"                    varchar(150)
-, "MiddleName"                   varchar(150)
-, "LastName"                     varchar(150)
-, "Suffix"                       varchar(10)
-, "Gender"                       varchar(25)
-, "PrimaryBusinessPosition"      varchar(150)
-, "Campus"                       varchar(150)
-, "PrimaryBusinessOrgName"       varchar(150)
-, "PrimaryBusinessAddress"       varchar(150)
-, "HIRING_LOCATION"              varchar(150)
-, "HIRING_LOCATION_DESC"         varchar(150)
-, "PrimaryBusinessCity"          varchar(150)
-, "PrimaryBusinessState"         varchar(10)
-, "PrimaryBusinessZip"           varchar(10)
-, "EMAIL_PREFERRED_ADDRESS"      varchar(150)
-, "StudentEmployee?"             varchar(3)
-, "EmployeeStatus"               varchar(25)
-, "Job Type"                     varchar(25)
-, "OSU Grad Self Rpt"            varchar(3)
-, "Retiree"                      varchar(3)
-, "Separated"                    date 
-, "LastUpdateDate"               date
-);
+    "Organization ID" varchar(50),                
+    "Organization Name" varchar(500),             
+    "Organization Type" varchar(100),             
+    "Organization Status" varchar(50),            
+    "Position Name" varchar(500),                 
+    "Position Template" varchar(50),              
+    "Position Start Date" DATE,                   
+    "Position End Date" DATE,                     
+    "Username" varchar(50),                       
+    "First Name" varchar(100),                    
+	"Preferred First Name" varchar(100),
+    "Middle Name Or Initial" varchar(100),        
+    "Last Name" varchar(100),                     
+    "Archived Username" varchar(100),             
+    "Suffix" varchar(100),                        
+    "Hometown" varchar(100),                      
+    "Affiliation" varchar(100),                   
+    "Campus Email" varchar(100),                  
+    "Preferred Email" varchar(100),               
+    "Card ID" varchar(100),                       
+    "SIS ID" varchar(100),                        
+    "Reflection" varchar(100),                    
+    "Date Of Birth" DATE,                         
+    "Sex" varchar(100),                           
+    "Race" varchar(100),                          
+    "Ethnicity" varchar(100),                     
+    "Enrollment Status" varchar(500),             
+    "Current Term Enrolled" varchar(500),         
+    "Current Term GPA" varchar(500),              
+    "Previous Term Enrolled" varchar(500),        
+    "Previous Term GPA" varchar(500),             
+    "Credit Hours Earned" varchar(500),           
+    "Anticipated Date Of Graduation" varchar(500),
+    "Career Level" varchar(500),                  
+    "Class Standing" varchar(500),                
+    "Primary School Of Enrollment" varchar(500),  
+    "Degree Sought" varchar(500),                 
+    "Major" varchar(500),                         
+    "Minor" varchar(500),                         
+    "Major Advisor" varchar(500),                 
+    "Other Advisor" varchar(500)                  
+);                                                
 
-COPY INTO "01_NewData" FROM '@STAGE/FoundationFacultyStaffFile.csv.gz'
+
+
+COPY INTO "01_NewData" FROM '@STAGE/SnowflakeUploadFile_Involvements.csv.gz'
 FORCE = TRUE
 PURGE = TRUE
-FILE_FORMAT = (FORMAT_NAME = 'CSV_DBLQT');
+FILE_FORMAT = (FORMAT_NAME = 'CSV_DBLQT_SKIP3');
 
 
 //Delete all files from STAGE
