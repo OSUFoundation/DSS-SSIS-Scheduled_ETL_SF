1010321deac74838970d0107df0f3b7c57819e9c
cecameron <ccameron@osugiving.com>
2021-05-29 11:50:27 -0500

    Update FENXT Snowflake DW sync

---
+++ FENXT_DW/SQL/DIM_Project_ApplyDelta.sql
@@ -0,0 +1,23 @@
+DELETE FROM "FENXT_DW"."BB"."DIM_PROJECT"
+WHERE "ProjectDimID" IN(SELECT "ProjectDimID" From "FENXT_DW"."BB_DELTA"."DIM_PROJECT");
+
+Insert Into "FENXT_DW"."BB"."DIM_PROJECT"
+Select 
+  "ProjectDimID"
+, "ProjectSystemID"
+, "ProjectDescription"
+, "ProjectStatusID"
+, "ProjectStatusDescription"
+, "ProjectTypeID"
+, "ProjectTypeDescription"
+, "ProjectStartDate"
+, "ProjectEndDate"
+, "ProjectActive"
+, "DateAdded"
+, "DateChanged"
+, "AnnotationText"
+, "ProjectID"
+, "ImportID"
+From "FENXT_DW"."BB_DELTA"."DIM_PROJECT"
+WHERE "UpdatedStatus" In('New','Modified');
+
