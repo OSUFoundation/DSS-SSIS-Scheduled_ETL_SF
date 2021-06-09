86f9e09d9d893aa9685585a7c2c911cd9d1a5ef1
David Fair <dfair@osugiving.com>
2021-06-09 15:51:17 -0500

    Added Dim_Class

---
+++ FENXT_DW/SQL/DIM_Class_copyAllFromStage.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE TABLE "FENXT_DW"."BB"."DIM_Class"
+
+(	
+	ClassDimID  	NUMBER(38,0)
+	,ClassSystemID 	NUMBER(38,0)
+	,ClassCode      NUMBER(38,0)
+	,ClassName      VARCHAR(256)
+	,ClassType      VARCHAR(256)
+	,Sequence       NUMBER(38,0)
+	,SourceID       NUMBER(38,0)
+	,ETLControlID   NUMBER(38,0)
+);
+
+COPY INTO "FENXT_DW"."BB"."DIM_Class" FROM '@BB_DELTA_STAGE/DIM_ClassAll.csv.gz'
+FORCE = TRUE
+PURGE = TRUE
+FILE_FORMAT = (FORMAT_NAME = 'CSV_DBLQT');
+
+
+//Delete all files from STAGE
+REMOVE '@BB_DELTA_STAGE';
+
---
+++ FENXT_DW/SQL/DIM_Class_putAllOnStage.sql
@@ -0,0 +1,2 @@
+put file://E:\BI/Scheduled_ETL_SF/FENXT_DW/DataFiles/DIM_ClassAll.csv @BB_DELTA_STAGE   OVERWRITE = TRUE  ;
+
